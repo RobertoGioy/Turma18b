d0662f4112fdf3fa4ac3857f21f25764d4827e9a
RobertoGioy <jroberto@live.com>
2021-05-20 08:53:03 -0300

    commit front end

---
+++ gama/js/funcao.js
@@ -0,0 +1,15 @@
+/* função de alert 
+       alert("Sejam todos bem-vindos ao Gama Academy!") */
+
+/* 
+var empresa = "Gama Academy";
+var mensagem = "Sejam bem-vindos à ";
+alert(mensagem+empresa); */
+
+/*
+var nome = prompt("Olá seja bem vindo à Gama Academy ")
+alert(nome) */
+
+function mensagem() {
+    alert("Para maiores informações acesse www.gamaacademy.com")
+}
